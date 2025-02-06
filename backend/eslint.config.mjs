8450e34d875bf77220d8fe6a59f2d24be88d57c3
peration0422 <patrick.jee@kakaocorp.com>
2025-02-06 10:44:03 +0900

    BE/fix : eslint 수정

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -29,7 +29,63 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'prefer-template': 'error',
+      eqeqeq: 'error',
+      'prefer-arrow-callback': 'error',
+      'no-plusplus': 'error',
+      'no-unneeded-ternary': 'error',
+      'no-prototype-builtins': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'interface',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'typeAlias',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'variable',
+          format: ['PascalCase', 'camelCase', 'UPPER_CASE'],
+        },
+        {
+          selector: 'variable',
+          types: ['function'],
+          format: ['PascalCase', 'camelCase'],
+        },
+        {
+          selector: 'variable',
+          types: ['boolean'],
+          format: ['PascalCase'],
+          prefix: ['is', 'has'],
+        },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: 'const', next: 'return' },
+      ],
+      'no-relative-import-paths/no-relative-import-paths': [
+        'error',
+        { allowSameFolder: false, rootDir: 'src', prefix: '@' },
+      ],
     },
   },
 );
